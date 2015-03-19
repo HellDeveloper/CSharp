34bd5babbb2ed0e0d459d17111c701235233e71e
Hell-Developer <hell.developer@outlook.com>
2015-03-19 17:21:00 +0800

    2015-03-19 17:20

--- Library/UnitTest/Program.cs
+++ Library/UnitTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utility.Core;
 
 namespace UnitTest
 {
@@ -11,8 +13,11 @@ namespace UnitTest
 
         public static void Main(string[] args)
         {
-
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.ReadKey();
         }
+
 
     }
 }
